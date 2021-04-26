55b855c6ac1ad1c1598f0842b4d73d2601f9f280
George <gknapasjo@gmail.com>
2021-04-26 11:39:17 +1000

    springboot database template added

---
+++ back-end/BlogDatabase/src/test/kotlin/com/example/BlogDatabase/BlogDatabaseApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.BlogDatabase
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlogDatabaseApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
