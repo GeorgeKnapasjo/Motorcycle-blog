7a0e1aad85124e97d0cd491b9ed87aaba3480fbb
George <gknapasjo@gmail.com>
2021-07-03 15:23:33 +1000

    remove unused code

--- src/components/pages/Popular.js
+++ src/components/pages/Popular.js
@@ -11,7 +11,6 @@ const Popular = () => {
     useEffect(() => {
         getRequest('Blogs').then(it => setData(it))
     },[])
-    const description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat'
     return(<>
         <PageWrapper>
             <TestContainer>
